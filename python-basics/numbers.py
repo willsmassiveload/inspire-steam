a05a36dbb8c80469ecc83aa1ea1e04def4e8d998
willsmassiveload <williamk4bebe09@gmail.com>
2026-02-11 10:59:24 +0300

    program to add,subtract,divide,multiply

---
+++ python-basics/numbers.py
@@ -0,0 +1,17 @@
+#Name : William Karanja
+# Date : 11/02/2026
+# program to add,subtract,divide,multipy
+
+first_number = int (input("Enter the first number"))
+second_number = int (input("Enter the second number"))
+
+
+sum_numbers = first_number + second_number
+difference_numbers =  first_number - second_number
+quocient_numbers = first_number/ second_number
+product_numbers = first_number * second_number
+
+print(f"The sum of the numbers {sum_numbers}")
+print(f"The difference of the numbers {difference_numbers}")
+print(f"The quocient of the numbers {quocient_numbers}")
+print(f"The product of the numbers {product_numbers}")
---
+++ python-basics/user_input.py
@@ -0,0 +1,8 @@
+# Name : William Karanja
+# Date :11/02/2026
+
+# program to get user input
+
+name = input("What is your name?")
+
+print(name)
