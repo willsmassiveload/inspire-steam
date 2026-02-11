599888930f5cdcec40221ac91c98630a58468385
willsmassiveload <williamk4bebe09@gmail.com>
2026-02-11 09:43:24 +0300

    first commit

---
+++ python-basics/challenge 1.py
@@ -0,0 +1,9 @@
+university = "UON"
+grade = "A"
+favourite_course = "Law and Finance"
+cluster_points = 74
+
+print(f"My favourite university is {university}")
+print(f"I got a grade of {grade}")
+print(f"My favourite course is {favourite_course}")
+print(f"I got {cluster_points} cluster points")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+#This is a comment
+print("Hello from William")
+# city/town
+print("I am from Nakuru")
+# my hobbies
+print("I like to play football,travelling,swimming and playing table tennis")
+
+age= 25
+height= 1.25
+weight= 75
+
+print(f"I am {age} years old")
+print(f"I am aslo{height} metres tall")
+print(f"And weigh{weight} kgs")
