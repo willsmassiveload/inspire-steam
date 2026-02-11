f203540605b3d21b637894b854b5a112ef9ee529
willsmassiveload <williamk4bebe09@gmail.com>
2026-02-11 09:57:58 +0300

    program to add two numbers

---
+++ python-basics/datatypes.py
@@ -0,0 +1,10 @@
+#Name : William Karanja
+# Date : 11/02/2026
+# Program to add two numbers
+
+first_number = 49
+second_number = 67
+
+sum_numbers = first_number + second_number
+
+print("sum_numbers")
