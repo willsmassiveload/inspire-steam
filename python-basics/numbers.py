fb2e16ad0c6fa6ed55c2055f4de1c7525eed565a
willsmassiveload <williamk4bebe09@gmail.com>
2026-02-12 10:36:06 +0300

    String formatting.py

--- python-basics/numbers.py
+++ python-basics/numbers.py
@@ -2,16 +2,26 @@
 # Date : 11/02/2026
 # program to add,subtract,divide,multipy
 
-first_number = int (input("Enter the first number"))
-second_number = int (input("Enter the second number"))
+#first_number = int (input("Enter the first number"))
+#second_number = int (input("Enter the second number"))
 
 
-sum_numbers = first_number + second_number
-difference_numbers =  first_number - second_number
-quocient_numbers = first_number/ second_number
-product_numbers = first_number * second_number
+#sum_numbers = first_number + second_number
+#difference_numbers =  first_number - second_number
+#quocient_numbers = first_number/ second_number
+#product_numbers = first_number * second_number
 
-print(f"The sum of the numbers {sum_numbers}")
-print(f"The difference of the numbers {difference_numbers}")
-print(f"The quocient of the numbers {quocient_numbers}")
-print(f"The product of the numbers {product_numbers}")
+#print(f"The sum of the numbers {sum_numbers}")
+#print(f"The difference of the numbers {difference_numbers}")
+#print(f"The quocient of the numbers {quocient_numbers}")
+#print(f"The product of the numbers {product_numbers}")
+
+
+third_number = input("What is the third number")
+
+print(f"The of third number is:{type(third_number)}")
+
+#Convert third number to an integer
+third_number = int(third_number)
+
+print(f"The type of third number is:{type(third_number)}") 
