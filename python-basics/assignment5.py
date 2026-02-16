715b93c5e78c35b62d16d5cfd28516885608f0e7
willsmassiveload <williamk4bebe09@gmail.com>
2026-02-16 22:42:22 +0300

    assignment5

--- python-basics/assignment5.py
+++ python-basics/assignment5.py
@@ -2,12 +2,19 @@
 # Date : 16/02/2026
 # Program to calculate income tax
 
-salary = int(input("Enter your gross salary"))
+salary = int(input("Enter your gross salary:"))
 
-if salary < 50000 :
-    tax = (2.5 * salary) / 100
-    net_salary = salary - tax
 
+
+if salary < 50000:
+    tax_rate = 2.5
+elif salary <= 100000:
+    tax_rate = 4.5
+else:
+    tax_rate = 7.5
+tax = (tax_rate * salary) / 100
+net_salary = salary - tax
 print(f"Gross salary = {salary}")
-print(f"Net salary = {net_salary}") 
-print (f"Tax = {tax}") 
+print(f"Tax Rate Applied = {tax_rate}%") 
+print (f"Tax = {tax}")
+print(f"Net salary = {net_salary}")
